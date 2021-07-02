bea564f5e74c97188d8684abf60392c10b762d70
Z3r0cks <patrick.kaserer@web.de>
2021-07-02 08:30:40 +0200

    FIX: style, questions

--- include/Pages.h
+++ include/Pages.h
@@ -6,19 +6,19 @@ const char masterIndex[] = R"=====(
 )=====";
 
 const char masterQuestions[] = R"=====(
-<div class="wrapper"><div class="scoreWrapper"><div class="score">GM <span id="scoreM">$SCORE_MASTER</span>: <span id="scoreP">$SCORE_PLAYER</span>P </div></div><div class="headerWrapper questionHeader"><h3 class="header">Wähle eine Kategorie aus</h1></div><div class="categories"><div class="catBlockWrapper"><div class="catWrapper"><a class="cat" href="#cat1">Flora</a></div><div class="catWrapper"><a class="cat" href="#cat2">Fauna</a></div></div><div class="catBlockWrapper"><div class="catWrapper"><a class="cat" href="#cat3">FuWa Things</a></div><div class="catWrapper"><a class="cat" href="#cat4">Berge und Flüsse</a></div></div></div><div id="cat1"><div class="catTitleWrapper"><h3 class="catText">Flora</h3></div></div><div id="cat2"><div class="catTitleWrapper"><h3 class="catText">Fauna</h3></div></div><div id="cat3"><div class="catTitleWrapper"><h3 class="catText">FuWa Things</h3></div></div><div id="cat4"><div class="catTitleWrapper"><h3 class="catText">Berge und Flüsse</h3></div></div></div>
+ <div class="wrapper"><div class="scoreWrapper"><div class="score">GM&nbsp;<span id="scoreM">$SCORE_MASTER</span>&nbsp;:&nbsp;<span id="scoreP">$SCORE_PLAYER</span>&nbsp;P</div></div><div class="headerWrapper questionHeader"><h3 class="header">Wähle eine Kategorie aus</h1></div><div class="categories"><div class="catBlockWrapper"><div class="catWrapper"><a class="cat" href="#cat1">Flora</a></div><div class="catWrapper"><a class="cat" href="#cat2">Fauna</a></div></div><div class="catBlockWrapper"><div class="catWrapper"><a class="cat" href="#cat3">FuWa Things</a></div><div class="catWrapper"><a class="cat" href="#cat4">Berge und Flüsse</a></div></div></div><div id="cat1"><div class="catTitleWrapper"><h3 class="catText">Flora</h3></div></div><div id="cat2"><div class="catTitleWrapper"><h3 class="catText">Fauna</h3></div></div><div id="cat3"><div class="catTitleWrapper"><h3 class="catText">FuWa Things</h3></div></div><div id="cat4"><div class="catTitleWrapper"><h3 class="catText">Berge und Flüsse</h3></div></div></div>
 )=====";
 
 const char masterSolution[] = R"=====(
-<div class="wrapper"><div class="scoreWrapper"><div class="score">GM <span id="scoreM">$SCORE_MASTER</span>: <span id="scoreP">$SCORE_PLAYER</span>P </div></div><div class="headerWrapper solutionWrapper"><p class="solutionText" id="solution"></p></div><div class="headerWrapper solutionWrapper2"><p class="solutionText" id="wait-info"></p></div></div>
+ <div class="wrapper"><div class="scoreWrapper"><div class="score">GM&nbsp;<span id="scoreM">$SCORE_MASTER</span>&nbsp;:&nbsp;<span id="scoreP">$SCORE_PLAYER</span>&nbsp;P</div></div><div class="headerWrapper solutionWrapper"><p class="solutionText" id="solution"></p></div><div class="headerWrapper solutionWrapper2"><p class="solutionText" id="wait-info"></p></div></div>
 )=====";
 
 const char masterVictory[] = R"=====(
- <div class="wrapper"><div class="headerWrapper"><h3 class="header siegHeader">Sieg </h3><p>Das Quiz beendet sich in 10 Sekunde, du kansch weggehe</p></div></div>
+ <div class="wrapper"><div class="headerWrapper"><h3 class="header siegHeader">Sieg </h3><p>Das Quiz beendet sich in 10 Sekunden</p></div></div>
 )=====";
 
 const char masterDefeat[] = R"=====(
-  <div class="wrapper"><div class="headerWrapper"><h3 class="header siegHeader">Leider verloren</h3><p>Das Quiz beendet sich in 10 Sekunde, du kansch weggehe</p></div></div>
+  <div class="wrapper"><div class="headerWrapper"><h3 class="header siegHeader">Leider verloren</h3><p>Das Quiz beendet sich in 10 Sekunden</p></div></div>
 )=====";
 
 const char masterError[] = R"=====(
